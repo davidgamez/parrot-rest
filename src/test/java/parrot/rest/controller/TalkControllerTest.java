c95cacaa85b3138440ede91a98131c09a85f8dfa
Isuru Weerasooriya <idweerasooriya@gmail.com>
2019-01-02 16:33:28 -0500

    fixed formatting

--- src/test/java/parrot/rest/controller/TalkControllerTest.java
+++ src/test/java/parrot/rest/controller/TalkControllerTest.java
@@ -1,6 +1,5 @@
 package parrot.rest.controller;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,58 +11,51 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.web.servlet.HandlerMapping;
 
+import parrot.rest.common.FixtureUtility;
 import parrot.rest.exception.UrlNotFoundException;
 import parrot.rest.service.PhraseService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TalkControllerTest {
 
-  @InjectMocks
-  TalkController fixture;
+	@InjectMocks
+	TalkController fixture;
 
-  @Mock
-  PhraseService phraseService;
+	@Mock
+	PhraseService phraseService;
 
-  @Test
-  public void testTalkExistingUrl() throws UrlNotFoundException {
-    HttpServletRequest request = getMockRequest("talk/appContext/url", "");
+	@Test
+	public void testTalkExistingUrl() throws UrlNotFoundException {
+		HttpServletRequest request = FixtureUtility.getMockRequest("talk/appContext/url", "");
 
-    fixture.talk(request);
+		fixture.talk(request);
 
-    verify(phraseService, times(1)).getResponse("appContext/url");
-  }
+		verify(phraseService, times(1)).getResponse("appContext/url");
+	}
 
-  @Test(expected = UrlNotFoundException.class)
-  public void testTalkBadUrlRequest() throws UrlNotFoundException {
-    HttpServletRequest request = getMockRequest("talk", "");
+	@Test(expected = UrlNotFoundException.class)
+	public void testTalkBadUrlRequest() throws UrlNotFoundException {
+		HttpServletRequest request = FixtureUtility.getMockRequest("talk", "");
 
-    fixture.talk(request);
-  }
+		fixture.talk(request);
+	}
 
-  @SuppressWarnings("unchecked")
-  @Test(expected = UrlNotFoundException.class)
-  public void testTalkNotExistingUrl() throws UrlNotFoundException {
-    HttpServletRequest request = getMockRequest("talk/appContext/url", "");
+	@SuppressWarnings("unchecked")
+	@Test(expected = UrlNotFoundException.class)
+	public void testTalkNotExistingUrl() throws UrlNotFoundException {
+		HttpServletRequest request = FixtureUtility.getMockRequest("talk/appContext/url", "");
 
-    when(phraseService.getResponse("appContext/url")).thenThrow(UrlNotFoundException.class);
-    fixture.talk(request);
-  }
+		when(phraseService.getResponse("appContext/url")).thenThrow(UrlNotFoundException.class);
+		fixture.talk(request);
+	}
 
-  @SuppressWarnings("unchecked")
-  @Test(expected = UrlNotFoundException.class)
-  public void testTalkEmptyUrl() throws UrlNotFoundException {
-    HttpServletRequest request = getMockRequest("", "");
+	@SuppressWarnings("unchecked")
+	@Test(expected = UrlNotFoundException.class)
+	public void testTalkEmptyUrl() throws UrlNotFoundException {
+		HttpServletRequest request = FixtureUtility.getMockRequest("", "");
 
-    when(phraseService.getResponse("appContext/url")).thenThrow(UrlNotFoundException.class);
-    fixture.talk(request);
-  }
-
-  private HttpServletRequest getMockRequest(String url, String parameters) {
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    when(request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE)).thenReturn(url);
-    when(request.getQueryString()).thenReturn(parameters);
-    return request;
-  }
+		when(phraseService.getResponse("appContext/url")).thenThrow(UrlNotFoundException.class);
+		fixture.talk(request);
+	}
 }
