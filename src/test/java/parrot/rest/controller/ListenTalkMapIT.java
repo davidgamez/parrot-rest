cab32b5dfcc1e85b6cb88a097bdfd9a49ebe36a8
Isuru Weerasooriya <idweerasooriya@gmail.com>
2019-01-02 16:32:28 -0500

    fixed formatting

--- src/test/java/parrot/rest/controller/ListenTalkMapIT.java
+++ src/test/java/parrot/rest/controller/ListenTalkMapIT.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package parrot.rest.controller;
 
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -31,75 +28,69 @@ import parrot.rest.common.Phrase;
 @PropertySource("classpath:application-test-map.properties")
 public class ListenTalkMapIT extends BaseIntegrationController {
 
-  private static final String JSON_RESPONSE = "{\"id\": 1, \"name\": \"Test response\"}";
-  
-  @Autowired
-  ListenController listenController;
+	private static final String JSON_RESPONSE = "{\"id\": 1, \"name\": \"Test response\"}";
 
-  @Autowired
-  WebApplicationContext context;
-  
-  @Before
-  public void setup() {
-    initMocks(this);
-    this.mockMvc = webAppContextSetup(context)
-        .alwaysDo(print()).build();
-  }
-  
-  @After
-  public void validate() {
-    Mockito.validateMockitoUsage();
-  }
-  
-  @Test
-  public void testListenTalkBasic() throws Exception {
-    
-    Phrase phrase = new Phrase();
-    phrase.setAppContext("app_context");
-    phrase.setUrl("the_url");
-    phrase.setResponse(JSON_RESPONSE);
-    
-    Gson gson = new Gson();
-    String json = gson.toJson(phrase);
-    
-//    Listen
-    mockMvc.perform(post("/listen").contentType(MediaType.APPLICATION_JSON).content(json))
-        .andExpect(status().isOk())
-      .andExpect(content().string("OK"));
+	@Autowired
+	ListenController listenController;
 
-//    Talk
-    mockMvc.perform(get("/talk/app_context/the_url"))
-    .andExpect(status().isOk())
-  .andExpect(content().string(JSON_RESPONSE));    
-  }
+	@Autowired
+	WebApplicationContext context;
 
-  @Test
-  public void testListenTalkParameters() throws Exception {
-    
-    Phrase phrase = new Phrase();
-    phrase.setAppContext("app_context");
-    phrase.setUrl("the_url?param=true");
-    phrase.setResponse(JSON_RESPONSE);
-    
-    Gson gson = new Gson();
-    String json = gson.toJson(phrase);
-    
-//    Listen
-    mockMvc.perform(post("/listen").contentType(MediaType.APPLICATION_JSON).content(json))
-        .andExpect(status().isOk())
-      .andExpect(content().string("OK"));
+	@Before
+	public void setup() {
+		initMocks(this);
+		this.mockMvc = webAppContextSetup(context).alwaysDo(print()).build();
+	}
 
-//    Talk
-    mockMvc.perform(get("/talk/app_context/the_url?param=true"))
-    .andExpect(status().isOk())
-  .andExpect(content().string(JSON_RESPONSE));    
-  }
-  
-  @Test
-  public void testListenMissingUrl() throws Exception {
+	@After
+	public void validate() {
+		Mockito.validateMockitoUsage();
+	}
 
-//    Talk
-    mockMvc.perform(get("/talk/no_existing_app_context/the_url"))
-    .andExpect(status().isNotFound());    
-  }
+	@Test
+	public void testListenTalkBasic() throws Exception {
+
+		Phrase phrase = new Phrase();
+		phrase.setAppContext("app_context");
+		phrase.setUrl("the_url");
+		phrase.setResponse(JSON_RESPONSE);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(phrase);
+
+		//    Listen
+		mockMvc.perform(post("/listen").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk()).andExpect(content().string("OK"));
+
+		//    Talk
+		mockMvc.perform(get("/talk/app_context/the_url")).andExpect(status().isOk())
+				.andExpect(content().string(JSON_RESPONSE));
+	}
+
+	@Test
+	public void testListenTalkParameters() throws Exception {
+
+		Phrase phrase = new Phrase();
+		phrase.setAppContext("app_context");
+		phrase.setUrl("the_url?param=true");
+		phrase.setResponse(JSON_RESPONSE);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(phrase);
+
+		//    Listen
+		mockMvc.perform(post("/listen").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk()).andExpect(content().string("OK"));
+
+		//    Talk
+		mockMvc.perform(get("/talk/app_context/the_url?param=true")).andExpect(status().isOk())
+				.andExpect(content().string(JSON_RESPONSE));
+	}
+
+	@Test
+	public void testListenMissingUrl() throws Exception {
+
+		//    Talk
+		mockMvc.perform(get("/talk/no_existing_app_context/the_url")).andExpect(status().isNotFound());
+	}
 }
